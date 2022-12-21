858c3efa107c2405fa3d09cca3c0cff5e65db7d9
Sean <ray10255118@yahoo.com.tw>
2022-12-21 12:59:11 +0800

    sean12/21_13_58

--- ChineseChest1.py
+++ ChineseChest1.py
@@ -18,24 +18,32 @@ class Chess(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.transform.scale(pygame.image.load("images/chess1.png"),(45,45))
         self.rect = self.image.get_rect()
-        
+        self.row = row
+        self.col = col        
         self.rect.x = 35
         self.rect.y = 35
-        self.row = row
-        self.col = col
+
         
         self.isback=True
         self.isclick=False
         self.size=7
 
-    def isClicked():
-        12
+    def isClicked(self):
+        self.isclick=True
+        self.image=pygame.surface((0,255,0))
+
+    def notClicked(self):
+        self.isclick=False
+    
+
+
 
 
 
 # region 呼叫物件
 
 dots = [[0]*8 for i in range(4)]
+chess = [[0]*8 for i in range(4)]
 
 all_point = pygame.sprite.Group()
 
@@ -46,8 +54,16 @@ for i in range(4):
         dots[i][j].rect.y += 48*i
         all_point.add(dots[i][j])
 
-chess1=Chess(0,0)
-all_point.add(chess1)
+for i in range(4):
+    for j in range(8):
+        chess[i][j] = Chess(j, i)
+        chess[i][j].row=i
+        chess[i][j].col=j
+        chess[i][j].rect.x += 53*j
+        chess[i][j].rect.y += 48*i
+        all_point.add(chess[i][j])
+
+
 
 # endregion
 
@@ -75,6 +91,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
+
 
 
 # region 背景畫面
